4c11dd9e452153fe1563dd3402df5413711d7280
Valuta Maxim <maxval1991@gmail.com>
2018-08-14 22:10:04 +0300

    Fixed problems with links and credentials

--- src/index.js
+++ src/index.js
@@ -2,15 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
-import { createHttpLink } from "apollo-link-http";
 import Config from "../config/config.json";
 import App from "./app";
 
 const client = new ApolloClient({
-  link: createHttpLink({
-    uri: Config.apolloClient.uri,
+  fetchOptions: {
     credentials: "same-origin"
-  })
+  },
+  uri: Config.apolloClient.uri
 });
 
 ReactDOM.render(
